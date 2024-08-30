c06de77a11af5a09b26882e462a62cea76d55665
AnhDV <153249395+AnhDVCoder@users.noreply.github.com>
2024-08-30 15:46:39 +0700

    Add files via upload

--- convert.js
+++ convert.js
@@ -3,19 +3,23 @@ import * as fs from 'fs';
 import * as puppeteer from 'puppeteer';
 import * as yaml from 'js-yaml';
 import * as child_process from 'child_process';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 setTerminalTitle("Convert");
 
 
 
 const session = process.argv[2];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-var conf;
-try {
-    conf = yaml.load(fs.readFileSync('config.yml', 'utf8'));
-  } catch (e) {
-    console.log(e);
-}
+// var conf;
+// try {
+//     conf = yaml.load(fs.readFileSync('config.yml', 'utf8'));
+//   } catch (e) {
+//     console.log(e);
+// }
 
 console.log(datetime('datetime'));
 
@@ -94,7 +98,7 @@ async function run(session, id, id_ST, task){
 	const page=await browser.newPage();
 
 	try {
-		await page.goto(conf.Stable_Diffusion, {waitUntil: 'networkidle2'});
+		await page.goto('http://127.0.0.1:7860/', {waitUntil: 'networkidle2'});
 	} catch (error) {
 		printLog(datetime('time') + '[ERROR]: Lỗi tải trang [' + id_ST + '-' + task + ']', 'red');
 		updateTable('process_convert', 'in_process', 0, 'id', id);
@@ -104,26 +108,28 @@ async function run(session, id, id_ST, task){
 	}
 	
 	try {
-		//Chọn danh sách các khối không được phép
+		//Chọn Mine Diffusion
 		await page.waitForSelector('#tabs > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(7)', {visible: true});//, timeout: 0
 		await page.click('#tabs > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(7)');
-		await page.waitForSelector('#component-1584 > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(2)');
-		await page.click('#component-1584 > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(2)');
-		await page.click('#component-1574');
-		await page.click('#component-1584 > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(1)');
+
+		//Chọn danh sách các khối không được phép
+		// await page.waitForSelector('#component-1584 > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(2)');
+		// await page.click('#component-1584 > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(2)');
+		// await page.click('#component-1574');
+		// await page.click('#component-1584 > div.tab-nav.scroll-hide.svelte-kqij2n > button:nth-child(1)');
 		
 		//Chỉnh size schematic 700x700
-		await page.focus('#component-1554 > div.wrap.svelte-1cl284s > div > input');
-		await page.click('#component-1554 > div.wrap.svelte-1cl284s > div > input', { clickCount: 3 });
+		await page.focus('#component-3212 > div.wrap.svelte-1cl284s > div > input');
+		await page.click('#component-3212 > div.wrap.svelte-1cl284s > div > input', { clickCount: 3 });
 		await page.keyboard.type('350');
-		await page.focus('#component-1555 > div.wrap.svelte-1cl284s > div > input');
-		await page.click('#component-1555 > div.wrap.svelte-1cl284s > div > input', { clickCount: 3 });
+		await page.focus('#component-3213 > div.wrap.svelte-1cl284s > div > input');
+		await page.click('#component-3213 > div.wrap.svelte-1cl284s > div > input', { clickCount: 3 });
 		await page.keyboard.type('350');
 
 		//Để đường dẫn export vào thư mục game
-		await page.focus('#component-1560 > label > textarea');
-		await page.click('#component-1560 > label > textarea', { clickCount: 3 });
-		await page.keyboard.type('//' + conf.IP_Host + '/VN-in-Minecraft/litematics');
+		await page.focus('#component-3218 > label > textarea');
+		await page.click('#component-3218 > label > textarea', { clickCount: 3 });
+		await page.keyboard.type(__dirname + '/server/plugins/FastAsyncWorldEdit/schematics/');
 	} catch (error) {
 		printLog(datetime('time') + '[ERROR]: Lỗi không tìm/ấn được Element thanh điều hướng [' + id_ST + '-' + task + ']', 'red');
 		updateTable('process_convert', 'in_process', 0, 'id', id);
@@ -132,11 +138,11 @@ async function run(session, id, id_ST, task){
 		return;
 	}
 
-	//Upload image
+	//Upload ảnh
 	try {
-		const elementHandle = await page.$("#component-1543 > div.image-container.svelte-p3y7hu > div > input");
+		const elementHandle = await page.$("#component-3201 > div.image-container.svelte-p3y7hu > div > input");
 		await elementHandle.uploadFile('scans/'+ id_ST + '-' + task + '.png');
-		await page.waitForSelector('#component-1543 > div.image-container.svelte-p3y7hu > div > img', {timeout: 60000});
+		await page.waitForSelector('#component-3201 > div.image-container.svelte-p3y7hu > div > img', {timeout: 60000});
 	} catch (error) {
 		printLog(datetime('time') + '[ERROR]: Lỗi upload ảnh! [' + id_ST + '-' + task + ']', 'red');
 		updateTable('process_convert', 'in_process', 0, 'id', id);
@@ -148,9 +154,9 @@ async function run(session, id, id_ST, task){
 
 	//Điền tên export
 	try {
-		await page.focus('#component-1561 > label > textarea');
-		await page.click('#component-1561 > label > textarea', { clickCount: 3 });
-		await page.keyboard.type('' + id_ST + '-' + task);
+		await page.focus('#component-3219 > label > textarea');
+		await page.click('#component-3219 > label > textarea', { clickCount: 3 });
+		await page.keyboard.type(id_ST + '-' + task);
 	} catch (error) {
 		printLog(datetime('time') + '[ERROR]: Lỗi input! [' + id_ST + '-' + task + ']', 'red');
 		updateTable('process_convert', 'in_process', 0, 'id', id);
@@ -162,7 +168,7 @@ async function run(session, id, id_ST, task){
 
 	//Export
 	try {
-		await page.click('#component-1563');
+		await page.click('#component-3221');
 		checkRecord(id_ST, browser, session, task, id);
 	} catch (error) {
 		printLog(datetime('time') + '[ERROR]: Lỗi Export! [' + id_ST + '-' + task + ']', 'red');
@@ -175,7 +181,7 @@ async function run(session, id, id_ST, task){
 
 async function checkRecord(id_ST, browser, session, task, id){
     var count = 0;
-    while(checkExist(id_ST, task) == 0){
+    while(checkLExist(id_ST, task) == 0){
         if(count == 5){
             printLog(datetime('time') + '[INFO]: Không tìm thấy bản ghi! [' + id_ST + '-' + task + ']', 'white');
 			updateTable('process_convert', 'in_process', 0, 'id', id);
@@ -183,9 +189,9 @@ async function checkRecord(id_ST, browser, session, task, id){
 			auto_check();
             return;
         }
-        if(checkExist(id_ST, task) == 0){
+        if(checkLExist(id_ST, task) == 0){
             count += 1;
-            printLog(datetime('time') + '[INFO]: Kiểm tra bản ghi... [' + id_ST + '-' + task + '][' + count + ']', 'white');
+            printLog(datetime('time') + '[INFO]: Kiểm tra bản ghi L... [' + id_ST + '-' + task + '][' + count + ']', 'white');
         }
         await delay(10000);
     }
@@ -195,26 +201,51 @@ async function checkRecord(id_ST, browser, session, task, id){
 	CTSchem(id_ST, task, browser, session, id);
 }
 
-function checkExist(id_ST, task){
-    if(fs.existsSync('server/plugins/Fawe/litematics/' + id_ST + '-' + task + '.litematic')){
+//Check if file Litematic exist
+function checkLExist(id_ST, task){
+    if(fs.existsSync(__dirname + '/server/plugins/FastAsyncWorldEdit/schematics/' + id_ST + '-' + task + '.litematic')){
         return 1;
     }
     return 0;
 }
 
-async function CTSchem(id_ST, task, browser, session, order){
-	var child = child_process.exec('java -jar server/plugins/fawe/schematics/Lite2Edit.jar --cli ' + id_ST + '-' + task + '.litematic',
+//Kiểm tra file Schematic
+function checkSExist(id_ST, task){
+    if(fs.existsSync(__dirname + '/server/plugins/FastAsyncWorldEdit/schematics/' + id_ST + '-' + task + '.schem')){
+        return 1;
+    }
+    return 0;
+}
+
+async function CTSchem(id_ST, task, browser, session, id){
+	var count = 0;
+	var child = child_process.exec('java -jar ./server/plugins/FastAsyncWorldEdit/schematics/Lite2Edit.jar --convert ./server/plugins/FastAsyncWorldEdit/schematics/' + id_ST + '-' + task + '.litematic',
 		function (error, stdout, stderr){
-			printLog(datetime('time') + '[INFO]: Convert thành công sang Schematic [' + id_ST + '-' + task + ']', 'green');
-			updateTable('locations_minecraft', 'converted', 1, 'id', id_ST);
-			updateOPC('in_process', 0, order);
-			// removeOPC(id, session);
-			updateOPC('complete', 1, order);
 			if(error !== null){
 				console.log('exec error: ' + error);
 			}
 		}
 	);
+
+	while(checkSExist(id_ST, task) == 0){
+        if(count == 5){
+            printLog(datetime('time') + '[INFO]: Không tìm thấy bản ghi! [' + id_ST + '-' + task + ']', 'white');
+			updateTable('process_convert', 'in_process', 0, 'id', id);
+			await browser.close();
+			auto_check();
+            return;
+        }
+        if(checkSExist(id_ST, task) == 0){
+            count += 1;
+            printLog(datetime('time') + '[INFO]: Kiểm tra bản ghi S... [' + id_ST + '-' + task + '][' + count + ']', 'white');
+        }
+        await delay(10000);
+    }
+
+	printLog(datetime('time') + '[INFO]: Convert thành công sang Schematic [' + id_ST + '-' + task + ']', 'green');
+			updateTable('process_convert', 'in_process', 0, 'id', id);
+			updateTable('process_convert', 'complete', 1, 'id', id);
+	
 
 	await delay(1000);
 
@@ -235,29 +266,8 @@ async function CTSchem(id_ST, task, browser, session, order){
 }
 
 
-function insertPP(session, id_ST){
+function insertPP(session, id_ST, task){
 	var SQL  = "INSERT INTO process_place (session, id_ST, task) VALUES (" + session + ", " + id_ST + ", " + task + ")";
-    db.query(SQL, function (error) {
-        if (error){
-            throw error;
-        } 
-    });
-}
-
-
-
-function insertPS(session, id_ST, task){
-	var SQL = 'INSERT INTO process_scan (session, id_ST, task) VALUES (' + session + ', ' + id_ST + ', ' + task + ')';
-	db.query(SQL, function (error) {
-		if (error){
-			throw error;
-		}
-	});
-        
-}
-
-function updatePC(column, value, id){
-	var SQL  = "UPDATE process_convert SET " + column + " = " + value + " WHERE id = " + id;
     db.query(SQL, function (error) {
         if (error){
             throw error;
